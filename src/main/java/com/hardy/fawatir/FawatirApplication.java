a79f1373500e50727cb25edb115c9b02ae81bc4a
Hamza <hamza.elhadrachi@gmail.com>
2026-07-13 19:14:24 +0100

    initial commit

---
+++ src/main/java/com/hardy/fawatir/FawatirApplication.java
@@ -0,0 +1,13 @@
+package com.hardy.fawatir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawatirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FawatirApplication.class, args);
+    }
+
+}

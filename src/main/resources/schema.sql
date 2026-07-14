5d10467e6ef6f4c55e2f3ca072200947c601c1b2
Hamza El Hadrachi <42845309+hamzaelhadrachi@users.noreply.github.com>
2026-07-14 16:08:13 +0100

    Create schema and tables for user management
    This SQL script creates the schema and tables for user management, roles, events, and verification processes, following specified naming conventions and constraints.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,130 @@
+##############################################################
+##                                                          ##
+## Author: Hardy                                            ##
+## Date: 07/14/2026                                         ##
+## Version: 1.0                                             ##
+##                                                          ##
+##############################################################
+/*
+    --- General Rules ---
+    * use underscored_names instead of CamelCase
+    * Table Names should be plural
+    * spell out id fields [item_id instead of id]
+    * don't use embiguous column names
+    * name foreign key columns the same as the columns they refer to
+    * use Caps for all SQL queries
+ */
+
+
+CREATE SCHEMA IF NOT EXISTS fawatir;
+
+SET NAMES 'UTF8MB4';
+
+SET TIME_ZONE = '+1:00';
+
+USE fawatir;
+
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+   id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   first_name   VARCHAR(50) NOT NULL ,
+   last_name    VARCHAR(50) NOT NULL ,
+   email        VARCHAR(100) NOT NULL ,
+   password     VARCHAR(255) DEFAULT NULL,
+   adress       VARCHAR(255) DEFAULT NULL,
+   phone        VARCHAR(30) DEFAULT NULL,
+   title        VARCHAR(50) DEFAULT NULL,
+   bio          VARCHAR(255) DEFAULT NULL,
+   enabled      BOOLEAN DEFAULT FALSE,
+   non_locked   BOOLEAN DEFAULT FALSE,
+   using_mfa    BOOLEAN DEFAULT FALSE,
+   created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+   image_url    VARCHAR(255) DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+   CONSTRAINT UQ_Users_Email UNIQUE (email)
+);
+
+DROP TABLE IF EXISTS Roles;
+
+CREATE TABLE Roles(
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50) NOT NULL ,
+    permission VARCHAR(255) NOT NULL,
+    CONSTRAINT UQ_Users_Email UNIQUE (name)
+);
+
+DROP TABLE IF EXISTS UserRoles;
+
+CREATE TABLE UserRoles (
+   id        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   user_id   BIGINT UNSIGNED NOT NULL,
+   role_id   BIGINT UNSIGNED NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE  ON UPDATE CASCADE,
+   FOREIGN KEY (role_id) REFERENCES Roles(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+   CONSTRAINT UQ_UserRoles_User_id UNIQUE (user_id)
+);
+
+DROP TABLE IF EXISTS Events;
+
+CREATE TABLE Events(
+      id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      type       VARCHAR(50) NOT NULL CHECK ( type IN ('LOGIN_ATTEMPT','LOGIN_ATTEMPT_FAILURE','LOGIN_ATTEMPT_SUCCESS','PROFILE_UPDATE','PROFILE_PICTURE_UPDATE','ROLE_UPDATE','ACCOUNT_SETTINGS_UPDATE','PASSWORD_UPDATE','MFA_UPDATE')),
+      description VARCHAR(255) NOT NULL,
+      CONSTRAINT UQ_Events_Type UNIQUE (type)
+);
+
+DROP TABLE IF EXISTS UserEvents;
+
+CREATE TABLE UserEvents (
+    id        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    user_id   BIGINT UNSIGNED NOT NULL,
+    event_id   BIGINT UNSIGNED NOT NULL,
+    device VARCHAR(100) NOT NULL ,
+    ip_address VARCHAR(100) NOT NULL ,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE  ON UPDATE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES Events(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS AccountVerification;
+
+CREATE TABLE AccountVerification (
+    id        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    user_id   BIGINT UNSIGNED NOT NULL,
+    url       VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT UQ_AccountVerification_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_AccountVerification_Url UNIQUE (url)
+);
+
+DROP TABLE IF EXISTS ResetPasswordVerification;
+
+CREATE TABLE ResetPasswordVerification (
+    id             BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    user_id        BIGINT UNSIGNED NOT NULL,
+    url            VARCHAR(255) NOT NULL,
+    expiration_date DATETIME NOT NULL ,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT UQ_ResetPasswordVerification_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_ResetPasswordVerification_Url UNIQUE (url)
+);
+
+DROP TABLE IF EXISTS TwoFactorVerification;
+
+CREATE TABLE TwoFactorVerification (
+    id              BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    user_id         BIGINT UNSIGNED NOT NULL,
+    code             VARCHAR(10) NOT NULL,
+    expiration_date DATETIME NOT NULL ,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT UQ_TwoFactorVerification_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_TwoFactorVerification_Code UNIQUE (code)
+);
+
+
+
+
+
+
+
+
